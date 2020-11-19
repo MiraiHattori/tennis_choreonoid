a17e5007b4c541afd6f73cda740abd4a9f5c3d19
Mirai Hattori <mirai2045@gmail.com>
2020-11-19 12:52:24 +0900

    update

--- scripts/choreonoid_settings.sh
+++ scripts/choreonoid_settings.sh
@@ -6,63 +6,63 @@ if [ $# -ge 1 ]; then
     MOTION=$1
 fi
 if [ $MOTION = "forehand" ]; then
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/scripts/CnoidPyUtil.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 elif [ $MOTION = "punch" ]; then
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/config/JAXON_RED_PUNCH.cnoid.in $(rospack find hrpsys_choreonoid_tutorials)/config/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 elif [ $MOTION = "kick" ]; then
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/config/JAXON_RED_KICK.cnoid.in $(rospack find hrpsys_choreonoid_tutorials)/config/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 elif [ $MOTION = "smash" ]; then
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/config/JAXON_RED_SMASH.cnoid.in $(rospack find hrpsys_choreonoid_tutorials)/config/.
     cp $(rospack find tennis_choroenoid)/scripts/CnoidPyUtil.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
     cp $(rospack find tennis_choroenoid)/scripts/moving_ball.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 elif [ $MOTION = "batting" ]; then
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC_WITH_BAT.wrl $(rospack find jvrc_models)/JAXON_JVRC/LARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC_WITH_BAT.wrl $(rospack find jvrc_models)/JAXON_JVRC/LARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/config/JAXON_RED_BATTING.cnoid.in $(rospack find hrpsys_choreonoid_tutorials)/config/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 elif [ $MOTION = "forehand-step" ]; then
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/config/JAXON_RED_FOREHAND-STEP.cnoid.in $(rospack find hrpsys_choreonoid_tutorials)/config/.
     cp $(rospack find tennis_choroenoid)/scripts/CnoidPyUtil.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
     cp $(rospack find tennis_choroenoid)/scripts/moving_ball.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 elif [ $MOTION = "volley-step" ]; then
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC_WITH_RACKET.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/RARM_LINK7_JVRC.wrl
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/scripts/CnoidPyUtil.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 else
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
-    cp $(rospack find tennis_choroenoid)/model/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/RARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/.
+    cp $(rospack find tennis_choroenoid)/models/LARM_LINK7_JVRC.wrl $(rospack find jvrc_models)/JAXON_JVRC/convex_hull/.
     cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
 fi
 cp $(rospack find tennis_choroenoid)/scripts/jaxon_red_setup.py $(rospack find hrpsys_choreonoid_tutorials)/scripts/.
